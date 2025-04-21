248b5d1c427542cea063d18dd19ecb558fca64e9
Sury Massiel Jaen Pérez <jaensury927@gmail.com>
2025-04-21 07:28:55 -0600

    Commit inicial a la tabla de pagina web

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+function active(demostracion)
+{
+
+    let boton = document.getElementById("demostracion").innerHTML = Date();
+
+}
+
+
+function libro(){
+    window.location.href = "https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup"; 
+    console.log("exito");
+}
